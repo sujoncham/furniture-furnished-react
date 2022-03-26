1a2fdec12d8e19708f926f129a63887409df3bbe
sujon-chambugong <sujonchambugong@gmail.com>
2022-03-27 03:42:03 +0600

    Answering Question

--- src/Description/Description.js
+++ src/Description/Description.js
@@ -4,15 +4,25 @@ const Description = () => {
     return (
         <div className='description'>
             <div className='question1 answer'>
-                <h2>How Reset works?</h2>
+                <h2>1. How Reset works?</h2>
                 <h3>Answer:</h3>
             </div>
             <div className='question2 answer'>
-                <h2>props vs state?</h2>
+                <h2>2. props vs state?</h2>
                 <h3>Answer:</h3>
+                <p>Props:</p>
+                <ul>
+                    <li>Props are read only</li>
+                    <li>Props can not be modified</li>
+                </ul>
+                <p>State</p>
+                <ul>
+                    <li>State changes can be asynchronous</li>
+                    <li>State can be modified using this.setState</li>
+                </ul>
             </div>
             <div className='question3 answer'>
-                <h2>How useStae works?</h2>
+                <h2>3. How useStae works?</h2>
                 <h3>Answer:</h3>
             </div>
         </div>
