81df194518121667ad8456f445dfdd5104fa2b71
sujon-chambugong <sujonchambugong@gmail.com>
2022-04-11 18:18:33 +0600

    header link added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import Furniture from './Funriture/Furniture';
-import Header from './Header/Header';
+import Header from './Header/Header'
+import Home from './Pages/Home'
+import Shop from './Pages/Shop'
+import Blogs from './Pages/Blogs'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Page404 from './Pages/Page404'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <div className="App">
-        <div className='container'>
-        <Header title="Furniture Shop"></Header>
-
-        <Furniture></Furniture>
-        </div>
+      <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/shop" element={<Shop></Shop>}></Route>
+          <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="*" element={<Page404></Page404>}></Route>
+        </Routes>
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './Header.css';
 
-const Header = ({title}) => {
+const Header = () => {
     return (
-        <div className='header'>
-        
-        <h1>{title}</h1>
-        <h3>Cart</h3>
-        
-        
-        </div>
+        <header className='header'>
+            <h1>Furniture Furnished</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/shop">Shop</Link>
+                <Link to="/blogs">Blog</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
+        </header>
         );
     };
     
