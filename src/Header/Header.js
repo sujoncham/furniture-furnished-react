2406e9593285eacb7f7c61141af0dac0816de9d6
sujon-chambugong <sujonchambugong@gmail.com>
2022-04-15 22:51:35 +0600

    signup done

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Container, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className='header'>
-        
-        <Container>
-            <Row className='menubar'>
+  return (
+    <div className="header">
+      <Container>
+        <Row>
+          <div className="menubar">
             <h1>Header</h1>
+            <nav className="menu">
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/blogs">Blogs</Link>
+              <Link to="/order">Order</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/login">Login</Link>
+            </nav>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-<nav className='menu'>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/blogs">Blogs</Link>
-    <Link to="/order">Order</Link>
-    <Link to="/contact">Contact</Link>
-    <Link to="/login">Login</Link>
-</nav>
-            </Row>
-        </Container>
-        </div>
-        );
-    };
-    
-    export default Header;
+export default Header;
--- src/SignUpOut/SignUp.js
+++ src/SignUpOut/SignUp.js
@@ -1,56 +1,128 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
+import auth from "../Firebase/Firebase.init";
 
 const SignUp = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] =useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setCorfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [validated, setValidated] = useState(false);
+  const [registered, setRegistered] = useState(false);
 
-  const handleNameEvent = (event)=>{
-      setName(event.target.value);
-  }
-  const handleEmailEvent = (event)=>{
+  const handleNameEvent = (event) => {
+    setName(event.target.value);
+  };
+  const handleEmailEvent = (event) => {
     setEmail(event.target.value);
-  }
-  const handlePasswordEvent = (event)=>{
+  };
+  const handlePasswordEvent = (event) => {
     setPassword(event.target.value);
+  };
+
+  const handleRegistered = (event) =>{
+    setRegistered(event.target.checked);
   }
 
-  const handleFormSubmit = (event)=>{
+  let passwordText;
+  const handleFormSubmit = (event) => {
     event.preventDefault();
-  }
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
 
+    if (password.length <= 6) {
+      passwordText = "password not less than 6 characters";
+    } else {
+      passwordText = "password ok";
+    }
 
+    if (!/(?=.*[A-Z])/.test(password)) {
+      setError("please one upper character provided");
+      return;
+    }
+    setValidated(true);
+    setError("");
+
+    console.log("form submitted", name, confirmPassword);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setEmail("");
+        setPassword("");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
 
   return (
     <Row className="mt-5">
       <Col md={{ span: 6, offset: 3 }}>
-        <h1>SignUp</h1>
-        <Form onSubmit={handleFormSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicName">
+        <h1>Please, {registered ? 'Login' : 'Register'}</h1>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicName">
             <Form.Label>Your name</Form.Label>
-            <Form.Control onBlur={handleNameEvent} type="text" name="name" placeholder="Enter name" />
+            <Form.Control
+              onBlur={handleNameEvent}
+              type="text"
+              name="name"
+              placeholder="Enter name"
+              required
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmailEvent}  type="email" name="email" placeholder="Enter email" />
+            <Form.Control
+              onBlur={handleEmailEvent}
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              required
+            />
           </Form.Group>
+          <Form.Control.Feedback type="invalid">
+            Please choose a username.
+          </Form.Control.Feedback>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePasswordEvent}  type="password" name="password" placeholder="Enter password" />
+            <Form.Control
+              onBlur={handlePasswordEvent}
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              required
+            />
           </Form.Group>
+          <p className="text-danger">{error}</p>
+          <p className="text-danger">{passwordText}</p>
+          <Form.Control.Feedback type="invalid">
+            Please choose a username.
+          </Form.Control.Feedback>
           <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
             <Form.Label>Confirm password</Form.Label>
-            <Form.Control type="password" name="confirmPassword" placeholder="Confirm Password" />
+            <Form.Control
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check onChange={handleRegistered} type="checkbox" label="Already registered" />
           </Form.Group>
 
           <Button variant="primary" type="submit">
-            Create Account
+            Register
           </Button>
         </Form>
-      
       </Col>
     </Row>
   );
